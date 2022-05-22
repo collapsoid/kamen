49f2114e268cbf2642562eaa6303e3d6b205d057
collapsoid <collapsly@protonmail.com>
2022-05-22 12:21:04 +0000

    fixed dimensions for root page's slideshow

--- src/App/Main/Root/Slideshow/Slideshow.tsx
+++ src/App/Main/Root/Slideshow/Slideshow.tsx
@@ -8,12 +8,16 @@ import Controls from './Controls/Controls';
 export default function SlideShow() {
     const [page, setPage] = useState(1);
 
-    const photos = ['a', 'b', 'c'];
+    const photos = [
+        'https://picsum.photos/1600/900?random=1',
+        'https://picsum.photos/1600/900?random=2',
+        'https://picsum.photos/1600/900?random=3'
+    ];
 
     useEffect(() => {
         const timer = setInterval(() => {
             setPage(page => page + 1 > photos.length ? 1 : page + 1);
-        }, 3500);
+        }, 4000);
 
         return () => {
             clearInterval(timer);
@@ -22,6 +26,11 @@ export default function SlideShow() {
 
     return (
         <section className="main-slideshow">
+            <img
+                className="main-slideshow__image"
+                src={photos[page - 1]}
+                alt={'image-' + page}
+            />
             <Controls
                 photos={photos}
                 page={page}
